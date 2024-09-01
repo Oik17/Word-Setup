0500aa9b389d5d61d35aa987fe7d902070ea3ccb
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-01 12:09:10 +0530

    feat: specify subject

--- word.ps1
+++ word.ps1
@@ -1,16 +1,22 @@
+param (
+    [string]$SubjectName = "DA"  # Default value if no parameter is provided
+)
+
 $Word = New-Object -ComObject word.application
+
 $Word.Visible = $true
+
 $Document = $Word.Documents.Add()
 
-$Document.PageSetup.TopMargin = 72   
+$Document.PageSetup.TopMargin = 72
 $Document.PageSetup.BottomMargin = 72 
 $Document.PageSetup.LeftMargin = 72   
 $Document.PageSetup.RightMargin = 72  
 
 $Range = $Document.Content.Paragraphs.Add().Range
-$Range.Text = "DA"
+$Range.Text = $SubjectName+" DA"
 $Range.Style = "Heading 1"
-$Range.Font.Name="Abadi Extra Light"
+$Range.Font.Name = "Abadi Extra Light"
 $Range.ParagraphFormat.Alignment = [Microsoft.Office.Interop.Word.WdParagraphAlignment]::wdAlignParagraphCenter
 $Range.Font.Size = 32
 $Range.InsertParagraphAfter()
@@ -18,7 +24,7 @@ $Range.InsertParagraphAfter()
 $Range = $Document.Content.Paragraphs.Add().Range
 $Range.Text = "Akshat Gupta"
 $Range.Style = "Normal"
-$Range.Font.Name="Abadi Extra Light"
+$Range.Font.Name = "Abadi Extra Light"
 $Range.ParagraphFormat.Alignment = [Microsoft.Office.Interop.Word.WdParagraphAlignment]::wdAlignParagraphRight
 $Range.Font.Size = 28
 $Range.InsertParagraphAfter()
@@ -26,7 +32,7 @@ $Range.InsertParagraphAfter()
 $Range = $Document.Content.Paragraphs.Add().Range
 $Range.Text = "22BCE2173"
 $Range.Style = "Normal"
-$Range.Font.Name="Abadi Extra Light"
+$Range.Font.Name = "Abadi Extra Light"
 $Range.ParagraphFormat.Alignment = [Microsoft.Office.Interop.Word.WdParagraphAlignment]::wdAlignParagraphRight
 $Range.Font.Size = 28
 $Range.InsertParagraphAfter()
@@ -34,7 +40,7 @@ $Range.InsertParagraphAfter()
 $Range = $Document.Content.Paragraphs.Add().Range
 $Range.Text = "Question"
 $Range.Style = "Normal"
-$Range.Font.Name="Abadi Extra Light"
+$Range.Font.Name = "Abadi Extra Light"
 $Range.ParagraphFormat.Alignment = [Microsoft.Office.Interop.Word.WdParagraphAlignment]::wdAlignParagraphLeft
 $Range.Font.Size = 19
 $Range.InsertParagraphAfter()
@@ -42,11 +48,10 @@ $Range.InsertParagraphAfter()
 $Range = $Document.Content.Paragraphs.Add().Range
 $Range.Text = "Answer"
 $Range.Style = "Normal"
-$Range.Font.Name="Abadi Extra Light"
+$Range.Font.Name = "Abadi Extra Light"
 $Range.ParagraphFormat.Alignment = [Microsoft.Office.Interop.Word.WdParagraphAlignment]::wdAlignParagraphLeft
 $Range.Font.Size = 17
 $Range.InsertParagraphAfter()
-
 
 $Document.Sections.Item(1).Borders.Enable = $true
 
