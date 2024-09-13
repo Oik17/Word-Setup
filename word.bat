b1023167f45767095ee0771fc40f12d5865132fc
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-13 08:12:08 +0530

    fix: subject default

---
+++ word.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -File "C:\Users\Akshat\Desktop\Tests\wordSetup\word.ps1" %*
--- word.ps1
+++ word.ps1
@@ -1,5 +1,5 @@
 param (
-    [string]$subject = "DA",
+    [string]$subject = "",
     [string]$name ="Akshat Gupta",
     [string]$regno="22BCE2173"
 )
